221672626e23754b5492551ceeadcd68d188dda0
Alisher Tuychiyev <269974854+alisher-ds@users.noreply.github.com>
2026-09-10 19:34:50 +0500

    chore: prune 49 unused media files, dead components, and obsolete markdown notes

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-import { formatUzPhone } from "./phone";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export const formatPhoneNumber = formatUzPhone;
